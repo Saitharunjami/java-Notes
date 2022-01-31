f667af4e67e0a1d71c7561fbfcfbe2cca773986b
Jami Sai Tharun <98573953+Saitharunjami@users.noreply.github.com>
2022-01-31 23:09:53 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,19 @@
+ public class Main {
+
+    public static void main(String[] args) {
+
+
+int x=5;
+                for(int i=1; i<=x; i++){
+                    for(int j=1; j<=i; j++){
+                        int sum = i+j;
+                        if(sum % 2 == 0){
+                            System.out.print("1 ");
+                        }else {
+                            System.out.print("0 ");
+                        }
+                    }
+                    System.out.println();
+                    
+         }
+    }
